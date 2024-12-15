6a014d796da2bf5cc2b009da9736262ecabb190f
{Arthur Billebaut} <{arthur.billebaut@efrei.net}>
2024-12-15 16:18:29 +0100

    fin payment

--- payment/routes/payment.js
+++ payment/routes/payment.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello, Payment service!');
+  res.send({response: 'Payment accepted',ok:true}).status(200);
 });
 
 export default router;
