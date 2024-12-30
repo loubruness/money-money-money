62576f960e65f29fe50033e028290f61ac0eb399
paulinedavid <pauline.david@efrei.net>
2024-12-30 18:52:56 +0100

    init

---
+++ email/database/db_connection.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import knex from 'knex';
+
+dotenv.config();
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+  },
+  //debug: true,
+});
+
+export { db };
