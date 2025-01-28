fd2b575d95107ed35b6950ccc6528283b560b3a2
{Arthur Billebaut} <{arthur.billebaut@efrei.net}>
2025-01-28 14:07:29 +0100

    corrections dotenv

--- email/index.js
+++ email/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 
 app.use(express.json());
--- payment/index.js
+++ payment/index.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import payment from './routes/payment.js';
 import express from 'express';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
